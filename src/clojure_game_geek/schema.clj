188cf668b139f31f7e19e768df853452fc6a1dc4
Diogo Queiroz <diogo.qds97@gmail.com>
2022-03-26 17:39:17 -0300

    add GameRating entities

--- src/clojure_game_geek/schema.clj
+++ src/clojure_game_geek/schema.clj
@@ -5,6 +5,11 @@
             [com.stuartsierra.component :as component]
             [clojure.edn :as edn]))
 
+(defn resolve-element-by-id
+  [element-map context args value]
+  (let [{:keys [id]} args]
+    (get element-map id)))
+
 (defn resolver-game-by-id
   [games-map context args value]
   (let [{:keys [id]} args]
@@ -29,16 +34,47 @@
           {}
           (get data k)))
 
+(defn rating-summary
+  [cgg-data]
+  (fn [_ _ board-game]
+    (let [id      (:id board-game)
+          ratings (->> cgg-data
+                       :ratings
+                       (filter #(= id (:game_id %)))
+                       (map :rating))
+          n        (count ratings)]
+      {:count n
+       :average (if (zero? n)
+                  0
+                  (/ (apply + ratings)
+                     (float n)))})))
+
+(defn member-ratings
+  [ratings-map]
+  (fn [_ _ member]
+    (let [id (:id member)]
+      (filter #(= id (:member_id %)) ratings-map))))
+
+(defn game-rating->game
+  [games-map]
+  (fn [_ _ game-rating]
+    (get games-map (:game_id game-rating))))
+
 (defn resolver-map
   [component]
   (let [cgg-data      (-> (io/resource "cgg-data.edn")
                           slurp
                           edn/read-string)
         games-map     (entity-map cgg-data :games)
+        members-map   (entity-map cgg-data :members)
         designers-map (entity-map cgg-data :designers)]
-    {:query/game-by-id (partial resolver-game-by-id games-map)
-     :BoardGame/designers (partial resolve-board-game-designers designers-map)
-     :Designer/games (partial resolve-designer-games games-map)}))
+    {:query/game-by-id         (partial resolve-element-by-id games-map)
+     :query/member-by-id       (partial resolve-element-by-id members-map)
+     :BoardGame/designers      (partial resolve-board-game-designers designers-map)
+     :BoardGame/rating-summary (rating-summary cgg-data)
+     :GameRating/game          (game-rating->game games-map)
+     :Designer/games           (partial resolve-designer-games games-map)
+     :Member/ratings           (member-ratings (:ratings cgg-data))}))
 
 (defn load-schema
   [component]
