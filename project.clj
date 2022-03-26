16dabc8de6fc0a0940cac3fd8dbb8ee15ec72cc4
Diogo Queiroz <diogo.qds97@gmail.com>
2022-03-26 11:28:25 -0300

    first commit

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject clojure-game-geek "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A tiny BoardGameGeek clone written in Clojure with Lacinia"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [com.walmartlabs/lacinia-pedestal "1.0"]
+                 [io.aviso/logging "1.0"]]
+  :profiles {:dev {:resource-paths ["dev-resources"]}}
   :repl-options {:init-ns clojure-game-geek.core})
